064beadcce9a7f5925f3f30e7ae5ab75ca3ae02e
yusunjo <tnswh2023@gmail.com>
2020-12-19 16:43:21 +0900

    next start - pages

---
+++ front/pages/index.js
@@ -0,0 +1,15 @@
+import AppLayout from "../components/AppLayout";
+
+//Next 는 import React from 'react'이게 필요가 없다.
+const Home = () => {
+    return (
+        <AppLayout>
+            <div>
+                hello,Next!
+            </div>
+        </AppLayout>
+    )
+
+}
+
+export default Home;

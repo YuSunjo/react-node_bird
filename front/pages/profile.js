da11c491c87b1a562d5bbec0a4d50a76c4def67c
yusunjo <tnswh2023@gmail.com>
2020-12-20 11:53:10 +0900

    profile page

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import AppLayout from '../components/AppLayout'
-import Head from 'next/head'
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+import Head from 'next/head';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const profile = ()=> {
+
+    const followerList=[{nickname: 'sunjo'}, {nickname: 'sunjo2'}, {nickname: 'sunjo3'}, ];
+    const followingList=[{nickname: 'sunjo'}, {nickname: 'sunjo2'}, {nickname: 'sunjo3'}, ];
+
     return (
         <>
             <Head>
                 <title>프로필 | NodeBird</title>
             </Head>
             <AppLayout>
-                profile
+                <NicknameEditForm /> 
+                <FollowList header="팔로잉 목록" data={followingList}/>
+                <FollowList header="팔로워 목록" data={followerList}/>
             </AppLayout>
         </>
     )

d0e4ca7db213583eca1262999c569fe4d880057a
yusunjo <tnswh2023@gmail.com>
2020-12-24 10:02:52 +0900

    immer

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,4 +1,4 @@
-import {all, fork,takeLatest, call, delay, put} from 'redux-saga/effects';
+import {all, fork,takeLatest,delay, put} from 'redux-saga/effects';
 import {
     // FOLLOW_FAILURE,
     // FOLLOW_REQUEST,
--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,33 +1,26 @@
-//
-import {createWrapper} from 'next-redux-wrapper';
-import {applyMiddleware, compose, createStore} from 'redux';
-import createSagaMiddleware from 'redux-saga'
-import {composeWithDevTools} from 'redux-devtools-extension';
+//커스텀 미들웨어를 만든다. thunk도 이런식  ==> 비동기작업에서 처리 하는데 많이 사용
+import { applyMiddleware, createStore, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createWrapper } from 'next-redux-wrapper';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import reducer from '../reducers';
 import rootSaga from '../sagas';
 
-
-//커스텀 미들웨어를 만든다. thunk도 이런식  ==> 비동기작업에서 처리 하는데 많이 사용
-const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
-    console.log(action);
-    return next(action);
-}
-
-const configureStore = () => {
-    const sagaMiddleware = createSagaMiddleware();
-    const middlewares = [sagaMiddleware, loggerMiddleware];
-    const enhancer = process.env.NODE_ENV =='production'
-        ? compose(applyMiddleware(...middlewares))
-        : composeWithDevTools(applyMiddleware(...middlewares))
-    const store = createStore(reducer, enhancer);
-    store.sagaTask = sagaMiddleware.run(rootSaga);
-    
-    return store;
-
+const configureStore = (context) => {
+  console.log(context);
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(
+      applyMiddleware(...middlewares),
+    );
+  const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
 };
 
-const wrapper = createWrapper(configureStore, {
-    debug: process.env.NODE_ENV === 'development', 
-});
+const wrapper = createWrapper(configureStore, { debug: process.env.NODE_ENV === 'development' });
 
 export default wrapper;
